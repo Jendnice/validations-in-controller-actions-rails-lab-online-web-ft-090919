a54132c531a7550c95e7cbdf04a34051e7732211
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-12 20:48:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   
   validates :title, presence: true 
-  validates :category, inclusion: { in: %w(Fiction Nonfiction) }
+  validates :category, inclusion: { in: %w("Fiction" "Non-fiction") }
   validates :content, length: { minimum: 100 }
   
 end
