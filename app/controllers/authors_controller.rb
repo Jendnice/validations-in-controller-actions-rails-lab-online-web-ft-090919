3d2ca999e90b48c7c2f75edf381a38dfd2f3af7b
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-12 20:38:37 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,7 +11,8 @@ class AuthorsController < ApplicationController
     if @author.valid?
       @author.save
       redirect_to author_path(@author)
-      
+    else 
+      render: new 
     
     @author = Author.create!(author_params)
 
