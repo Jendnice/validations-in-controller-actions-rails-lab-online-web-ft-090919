5ec13b48f6647daeec46908a89fc130b87f97422
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-12 20:59:15 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,8 @@ class PostsController < ApplicationController
   def update
     # @post.update(post_params)
     # redirect_to post_path(@post)
+    
+    @post = Post.new(post_params)
     if @post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
