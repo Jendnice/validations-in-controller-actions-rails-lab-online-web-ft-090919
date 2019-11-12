e8f4d60441b1ce0d7fa9cccf4f790bc9bfc5c570
Jennifer Nicely <pipeline38@hotmail.com>
2019-11-12 20:34:26 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   
   validates :title, presence: true 
-  validates :category, inclusion { 
+  validates :category, inclusion: { 
   validates :content, length { minimum: 100 }
   
 end
